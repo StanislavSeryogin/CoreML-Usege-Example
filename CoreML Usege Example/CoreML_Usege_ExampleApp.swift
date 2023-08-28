8255277440ebdc2b9f25e53ac6748427ba7f49ee
Stanislav <stas.seryogin.87@gmail.com>
2023-08-28 15:58:31 +0300

    Initial Commit

---
+++ CoreML Usege Example/CoreML_Usege_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreML_Usege_ExampleApp.swift
+//  CoreML Usege Example
+//
+//  Created by Stanislav Seryogin on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreML_Usege_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
